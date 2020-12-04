d374b914bdc2b872a2f64e8246cd7206d0fe0eb7
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-04 17:35:26 -0600

    Added option to visualize trajectory output.

--- src/main/java/us/ihmc/robotics/math/trajectories/TrajectoryPointOptimizerVisualizer.java
+++ src/main/java/us/ihmc/robotics/math/trajectories/TrajectoryPointOptimizerVisualizer.java
@@ -21,12 +21,22 @@ import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class TrajectoryPointOptimizerVisualizer
 {
-   private enum TestType {
-      STRAIGHT,
-      SWING,
-      LINE,
-      SPHERE
+   /**
+    * <ul>
+    * <li>When {@code true}: setup SCS to visualize each iteration of the optimization as a simulation
+    * tick.
+    * <li>When {@code false}: discretize the optimized trajectory to visualize the output
+    * position/velocity/acceleration over time.
+    * <li>
+    * </ul>
+    */
+   private static final boolean VISUALIZE_CONVERGENCE = true;
+
+   private enum TestType
+   {
+      STRAIGHT, SWING, LINE, SPHERE
    }
+
    private static final TestType type = TestType.SWING;
    private static final int points = 10;
 
@@ -46,21 +56,21 @@ public class TrajectoryPointOptimizerVisualizer
    {
       switch (type)
       {
-      case SWING:
-         setupSwing();
-         break;
-      case LINE:
-         setupLine();
-         break;
-      case SPHERE:
-         setupSphere();
-         break;
-      default:
-         initialPosition.setIncludingFrame(worldFrame, -0.5, -0.5, 0.0);
-         initialVelocity.setIncludingFrame(worldFrame, 0.0, 0.0, 0.0);
-         finalPosition.setIncludingFrame(worldFrame, 1.0, 0.0, 1.0);
-         finalVelocity.setIncludingFrame(worldFrame, 0.0, 0.0, 0.0);
-         break;
+         case SWING:
+            setupSwing();
+            break;
+         case LINE:
+            setupLine();
+            break;
+         case SPHERE:
+            setupSphere();
+            break;
+         default:
+            initialPosition.setIncludingFrame(worldFrame, -0.5, -0.5, 0.0);
+            initialVelocity.setIncludingFrame(worldFrame, 0.0, 0.0, 0.0);
+            finalPosition.setIncludingFrame(worldFrame, 1.0, 0.0, 1.0);
+            finalVelocity.setIncludingFrame(worldFrame, 0.0, 0.0, 0.0);
+            break;
       }
 
       trajectory.setEndpointConditions(initialPosition, initialVelocity, finalPosition, finalVelocity);
@@ -72,14 +82,14 @@ public class TrajectoryPointOptimizerVisualizer
    {
       Point3D sphereOrigin = new Point3D(0.0, 0.0, 0.7);
       double sphereRadius = 0.5;
-      Point3D[] waypointPositions = SpiralBasedAlgorithm.generatePointsOnSphere(sphereOrigin, sphereRadius, points+2);
+      Point3D[] waypointPositions = SpiralBasedAlgorithm.generatePointsOnSphere(sphereOrigin, sphereRadius, points + 2);
 
       initialPosition.setIncludingFrame(worldFrame, waypointPositions[0]);
       initialVelocity.setIncludingFrame(worldFrame, 0.0, 0.0, 0.0);
-      finalPosition.setIncludingFrame(worldFrame, waypointPositions[points-1]);
+      finalPosition.setIncludingFrame(worldFrame, waypointPositions[points - 1]);
       finalVelocity.setIncludingFrame(worldFrame, 0.0, 0.0, 0.0);
 
-      for (int i = 1; i < points-1; i++)
+      for (int i = 1; i < points - 1; i++)
          this.waypointPositions.add(new FramePoint3D(worldFrame, waypointPositions[i]));
    }
 
@@ -92,7 +102,7 @@ public class TrajectoryPointOptimizerVisualizer
 
       for (int i = 0; i < points; i++)
       {
-         double percent = (double)(i+1) / (double)(points+1);
+         double percent = (double) (i + 1) / (double) (points + 1);
          FramePoint3D waypoint = new FramePoint3D();
          waypoint.interpolate(initialPosition, finalPosition, percent);
          waypointPositions.add(waypoint);
@@ -128,7 +138,10 @@ public class TrajectoryPointOptimizerVisualizer
 
       SimulationConstructionSetParameters parameters = new SimulationConstructionSetParameters();
       parameters.setCreateGUI(true);
-      parameters.setDataBufferSize(100);
+      if (VISUALIZE_CONVERGENCE)
+         parameters.setDataBufferSize(100);
+      else
+         parameters.setDataBufferSize(2048);
       SimulationConstructionSet scs = new SimulationConstructionSet(new Robot("dummy"), parameters);
       scs.addYoRegistry(registry);
       scs.setDT(1.0, 1);
@@ -143,9 +156,23 @@ public class TrajectoryPointOptimizerVisualizer
       while (!trajectory.hasConverged())
       {
          trajectory.compute(0.0);
-         scs.setTime(time);
-         scs.tickAndUpdate();
-         time += 1.0;
+         if (VISUALIZE_CONVERGENCE)
+         {
+            scs.setTime(time);
+            scs.tickAndUpdate();
+            time += 1.0;
+         }
+      }
+
+      if (!VISUALIZE_CONVERGENCE)
+      {
+         for (int i = 0; i < parameters.getDataBufferSize(); i++)
+         {
+            time = (double) i / (parameters.getDataBufferSize() - 1.0);
+            trajectory.compute(time);
+            scs.setTime(time);
+            scs.tickAndUpdate();
+         }
       }
 
       scs.startOnAThread();
